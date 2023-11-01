e2a84ca17f01eff6749ae3ba82aa690f88a3afee
Ngo Dinh Canh <rapidstars97@gmail.com>
2023-11-01 22:15:16 +0700

    day 11

--- learning_day_10/ex31.cpp
+++ learning_day_10/ex31.cpp
@@ -5,7 +5,8 @@ int main(){
     int a1, a2, a3, b1, b2, b3 , n; cin >> a1 >> a2 >> a3 >> b1 >> b2 >> b3 >> n;
     int kC = ceil((a1+ a2+ a3)/5);
     int kHc = ceil((b1+ b2+b3)/10);
-    if (kC + kHc <= n) cout << "YES" << endl;
+    cout << kC << " " << kHc << endl;
+    if (kC + kHc <= n && kC + kHc != 0) cout << "YES" << endl;
     else cout << "NO" << endl;
     return 0;
 }
